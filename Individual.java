e425afe4dd6c611605668acd146305c23cf8a809
AmandaShen <amanda@ip-123-31.wireless.oberlin.edu>
2019-12-04 18:10:53 -0500

    base class

---
+++ Individual.java
@@ -0,0 +1,73 @@
+// Amanda Shen 
+// inidividual class for genetic algorithm 
+import java.util.*;
+import java.math.*;
+
+public class Individual {
+    private int id;
+    private int[] chromosome;
+	private double fitness = -1;
+    
+    // constructor for input type : int[]
+    public Individual(int[] newchromosome) {
+		this.chromosome = newchromosome;
+    }
+    // constructor for input type : length
+    public Individual(int chromosomeLength) {
+        int[] temp = new int[chromosomeLength];
+        this.chromosome = RandomSetGene(temp);
+    }
+
+    public int[] RandomSetGene (int[] chromosome) {
+        for (int i = 0; i < chromosome.length; i++) {
+            double random = Math.random();
+            if (random < 0.5) {
+                chromosome[i] = 0;
+            } else {
+                chromosome[i] = 1;
+            }
+        }
+        return chromosome;
+    }
+
+    // getter and setter
+    public int[] getChromosome() {
+		return this.chromosome;
+    }
+
+    public int getID() {
+        return this.id;
+    }
+    
+    public int getChromosomeLength() {
+		return this.chromosome.length;
+    }
+    public int getGene(int index) {
+		return this.chromosome[index];
+    }
+    
+    public double getFitness() {
+		return this.fitness;
+    }
+    public void setID (int id) {
+        this.id = id;
+    }
+    
+    public void setGene(int index, int value) {
+		this.chromosome[index] = value;
+    }
+
+    public void setFitness(double fitness) {
+		this.fitness = fitness;
+    }
+
+    public String toString() {
+		String output = "";
+		for (int i = 0; i < this.chromosome.length; i++) {
+			output += this.chromosome[i];
+		}
+		return output;
+	}
+    
+ 
+}
---
+++ Population.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
+public class Population {
+    private Individual population[];
+    private double populationFitness = -1;
+    
+    public Population(int populationSize) {
+		this.population = new Individual[populationSize];
+    }
+    
+    public Population(int populationSize, int chromosomeLength) {
+		this.population = new Individual[populationSize];
+		for (int i = 0; i < populationSize; i++) {
+			Individual individual = new Individual(chromosomeLength); // return a random complete individual
+			this.population[i] = individual;
+		}
+    }
+    
+    public Individual[] getIndividuals() {
+		return this.population;
+    }
+
+    public Individual getFittest (int index) {
+        SortByFit();
+        return this.population[index];
+    }
+
+    public void SortByFit() {
+		Arrays.sort(this.population, new Comparator<Individual>() {
+			public int compare(Individual o1, Individual o2) {
+				if (o1.getFitness() > o2.getFitness()) {
+					return -1;
+				} else if (o1.getFitness() < o2.getFitness()) {
+					return 1;
+				}
+				return 0;
+			}
+        });
+    }
+
+    public double getPopulationFitness() {
+		return this.populationFitness;
+	}
+
+    public void setPopulationFitness(double fitness) {
+		this.populationFitness = fitness;
+    }
+    
+    public int size() {
+		return this.population.length;
+    }
+    
+    public Individual setIndividual(int index, Individual individual) {
+		return population[index] = individual;
+    }
+    
+    public Individual getIndividual(int index) {
+		return population[index];
+    }
+    
+    public void shuffle() {
+		Random rnd = new Random();
+		for (int i = population.length - 1; i > 0; i--) {
+			int index = rnd.nextInt(i + 1);
+			Individual a = population[index];
+			population[index] = population[i];
+			population[i] = a;
+		}
+	}
+
+}
